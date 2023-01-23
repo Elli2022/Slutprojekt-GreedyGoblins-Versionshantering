cd6f338de0bbb723ad21aec420750e1f4a9bb2de
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-01-23 18:14:59 +0100

    Laddar upp index, css och js filen

---
+++ script.js
@@ -0,0 +1,88 @@
+//Vad som ska importeras från Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
+
+//Allt relaterat till Firebase.
+const firebaseConfig = {
+  databaseURL:
+    "https://projekt1-97671-default-rtdb.europe-west1.firebasedatabase.app/",
+  apiKey: "AIzaSyDianSNbCGNVs8WJ35olsQP3fJt7JcZJec",
+  authDomain: "projekt1-97671.firebaseapp.com",
+  projectId: "projekt1-97671",
+  storageBucket: "projekt1-97671.appspot.com",
+  messagingSenderId: "125852933537",
+  appId: "1:125852933537:web:5c52ecb7495050a7e63fe0",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+console.log(db);
+
+//Funktionen
+let content = document.getElementById("content");
+let btn = document.getElementById("btn");
+let array = [];
+btn.addEventListener("click", () => {
+  writeUserData();
+  resetText();
+  onValue(
+    ref(db, "/"),
+    (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        let item = document.createElement("h4");
+        item.className = "headern";
+        item.setAttribute("name", childKey);
+        item.innerHTML = `${childKey} ${childData.message} ${childData.dateOfCreation} `;
+        content.appendChild(item);
+        array.push(item);
+      });
+      removeHeader();
+    },
+    { onlyOnce: true }
+  );
+});
+
+//Funktion för att skriva ut värden från input + datum
+function writeUserData() {
+  let inputName = document.getElementById("inName").value;
+  set(ref(db, inputName), {
+    message: "",
+    dateOfCreation: new Date().toLocaleDateString(),
+  });
+}
+
+//Rendera det på DOM
+const urlRef = ref(db, "Dany");
+onValue(
+  urlRef,
+  (snapshot) => {
+    const data = snapshot.val();
+  },
+  { onlyOnce: true }
+);
+
+//Funktion som rensar array + innerHTML
+function resetText() {
+  array = [];
+  content.innerHTML = "";
+}
+
+//Tar bort element från DOM & Databasen
+function removeHeader() {
+  array.forEach((element) => {
+    element.addEventListener("click", () => {
+      let name = element.getAttribute("name");
+      remove(ref(db, name)).then(() => {
+        element.remove();
+      });
+    });
+  });
+}
